2e1714b9ea5fbf39992677edef5fd3336ea02721
Slimm6 <garcia.andre@northeastern.edu>
2025-12-02 02:17:20 -0500

    set up test scene

--- View.cpp
+++ View.cpp
@@ -18,8 +18,8 @@ using namespace std;
 // active2, active3, active4 determines what camera mode you are currently in
 // first to manage when the text scengraph renderer prints.
 // tick is used for animation and active3 camera mode to determine the rate of movement.
-glm::vec3 stationaryLook = glm::vec3(110.0f,175.0f,125.0f);
-glm::vec3 stationaryTarget = glm::vec3(60.0f, 0.0f, -50.0f);
+glm::vec3 stationaryLook = glm::vec3(0.0f,0.0f,20.0f);
+glm::vec3 stationaryTarget = glm::vec3(0.0f, 0.0f, 0.0f);
 glm::vec3 look = stationaryLook;
 glm::vec3 target = stationaryTarget;
 map<string,unsigned int> textureIds;
@@ -205,7 +205,7 @@ void View::init(map<string,util::PolygonMesh<VertexAttrib>>& meshes, sgraph::ISc
 	projection = glm::perspective(glm::radians(60.0f),(float)window_width/window_height,0.1f,10000.0f);
     glViewport(0, 0, window_width,window_height);
     renderer = new sgraph::GLScenegraphRenderer(modelview,objects,shaderLocations);    
-    raytracer = new ray::Raytracer(800, 800, modelview, meshes, scenegraph);
+    raytracer = new ray::Raytracer(50, 50, modelview, meshes, scenegraph);
 }
 
 //Camera Mode 4 is handled here. Using the inverse of the renderer's animation to follow the plane.
